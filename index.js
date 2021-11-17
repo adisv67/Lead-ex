a24577b125876d1fce3bef3e471ebdeffedbf2f1
Adesh S <adesh.sonawane@morfsys.com>
2021-11-17 12:57:40 +0530

    css added

---
+++ index.js
@@ -0,0 +1,10 @@
+function saveLead(){
+    // console.log("button clicked")
+}
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click",function(){
+
+    console.log("button clicked from add event")
+
+})
