cc465643db4cb52b7e3a20a28e23381d0ab2da55
Adesh S <adesh.sonawane@morfsys.com>
2021-11-18 09:37:10 +0530

    list view added

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-function saveLead(){
-    // console.log("button clicked")
-}
-let inputBtn = document.getElementById("input-btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click",function(){
-
-    console.log("button clicked from add event")
-
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 })
+
+function renderLeads(){
+    let listItems = ""
+    for (let i = 0; i<myLeads.length; i++){
+        listItems += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems
+}
